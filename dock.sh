4348714f7dd47e1311102a22a4b3241693f9cc35
dah <52811389+davidhsin@users.noreply.github.com>
2025-10-26 00:37:39 +0800

    Add comments for mouse acceleration defaults
    Added comments about mouse acceleration settings.

--- dock.sh
+++ dock.sh
@@ -1,5 +1,8 @@
 #!/bin bash
 
+# Mouse Acceleration defaults is 0.6875
+# run: 'defaults read -g com.apple.mouse.scaling' to check it.
+
 defaults write com.apple.dock autohide-delay -int 0
 defaults write com.apple.dock autohide-time-modifier -float 0.45
 killall Dock
